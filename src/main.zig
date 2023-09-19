adabf089863bc6674ccf8dd76d38d229a22d5a08
WhyK <windchimeyk@gmail.com>
2023-09-20 00:12:38 +0900

    feat: learning language overview

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,13 @@ const MAX_POWER = userImports.MAX_POWER;
 
 // This code won't compile if `main` isn't `pub` (public)
 pub fn main() void {
+    const a: [5]i32 = .{ 1, 2, 3, 4, 5 };
+    var end: usize = 4;
+    const b = a[1..end];
+    std.debug.print("{any}\n", .{@TypeOf(b)});
+    const c = [3:false]bool{ false, true, false };
+    std.debug.print("{any}\n", .{std.mem.asBytes(&c).*});
+
     const user = User.init("Goku", 800 + User.SUPER_POWER);
 
     std.debug.print("{s}'s power is {d}\n", .{ user.name, user.power });
